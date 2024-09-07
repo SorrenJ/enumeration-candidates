c0c5863537c65e3ef23dc3de44303be5c7ebc4af
SorrenJ <soalexjao@gmail.com>
2024-09-06 23:42:58 -0700

    add find method and pass candidate block to check for the id

--- filters.rb
+++ filters.rb
@@ -2,18 +2,23 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+    # Returns the candidate with that :id
+    @candidates.find { |candidate| candidate[:id] == id }
+
+
   end
   
   def experienced?(candidate)
-
-    #  Takes an id, returns the candidate with that :id if there is no candidate with that id, naturally returns nil
+    # Takes an id, returns the candidate with that :id if there is no candidate with that id, naturally returns nil
+    # No output (puts)
+    
     candidate[:years_of_experience] >= 2
 
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    
+
   end
   
   # More methods will go below
